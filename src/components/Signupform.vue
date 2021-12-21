3c43bad5b3f3d073cd3a1598f840ed5f3094bc58
AhmedRabea0302 <arabea169@gmail.com>
2021-12-21 09:51:19 +0200

    first Commit

---
+++ src/components/Signupform.vue
@@ -0,0 +1,25 @@
+<template>
+  <form >
+      <label>This is for th email field</label>
+      <input type="email" required>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+form {
+    max-width: 420px;
+    margin: 30px auto;
+    background-color:white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+
+</style>
